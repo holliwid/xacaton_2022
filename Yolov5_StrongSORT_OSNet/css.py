4c8d010a2f6ed3ae8aab6f4f0d5f767a54f9354d
holliwid <stepales1111@gmail.com>
2022-10-14 04:55:52 +0300

    change style on video tab

--- Yolov5_StrongSORT_OSNet/css.py
+++ Yolov5_StrongSORT_OSNet/css.py
@@ -14,3 +14,21 @@ QPushButton:hover { \
   text-decoration: none;\
 }\
 "
+
+
+button_video = "QPushButton{\
+  background-color: lightgray;\
+  border: 1px solid rgba(27, 31, 35, 0.15);\
+  border-radius: 6px;\
+  color: black;\
+  font-size: 14px;\
+  font-weight: 500;\
+  line-height: 20px;\
+  list-style: none;\
+  padding: 1px 1px;\
+}\
+QPushButton:hover { \
+  background-color: lightgreen;\
+  text-decoration: none;\
+}\
+"
--- Yolov5_StrongSORT_OSNet/script.py
+++ Yolov5_StrongSORT_OSNet/script.py
@@ -7,6 +7,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import creat_graphics
 from heatmappy import Heatmapper
+import sqlite3
 
 
 from PIL import Image
